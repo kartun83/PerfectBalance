f405ae3f04f286b13b4cee9af942427736e561f6
Kartun <kartun@yandex.ru>
2021-12-03 13:56:24 +0300

    Initial commit

---
+++ wear/src/main/java/com/kartun/perfectbalance/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.kartun.perfectbalance
+
+import android.app.Activity
+import android.os.Bundle
+import com.kartun.perfectbalance.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
